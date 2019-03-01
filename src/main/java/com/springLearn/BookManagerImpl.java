de3a2b9b4db5c7b4e949626d97731459bb8672ab
zhou <280208673@qq.com>
2019-03-01 17:56:37 +0800

    spring 接口测试

---
+++ src/main/java/com/springLearn/BookManagerImpl.java
@@ -0,0 +1,18 @@
+package com.springLearn;
+
+public class BookManagerImpl implements  BookManger {
+    @Override
+    public void readBook() {
+
+    }
+
+    @Override
+    public int storeBooke(int num) {
+        return 0;
+    }
+
+    @Override
+    public boolean hashBook() {
+        return false;
+    }
+}
---
+++ src/main/java/com/springLearn/BookManger.java
@@ -0,0 +1,9 @@
+package com.springLearn;
+
+public interface BookManger {
+    void readBook();
+
+    int storeBooke(int num);
+
+    boolean hashBook();
+}
